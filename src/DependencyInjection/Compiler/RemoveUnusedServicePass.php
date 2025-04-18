1a0ba6c8788124a0f1dca0ce417160c00ec17b98
github-ci <you@example.com>
2025-04-18 15:09:50 +0000

    feat(symfony-async-bundle): 添加移除未使用服务的编译器传递

---
+++ src/DependencyInjection/Compiler/RemoveUnusedServicePass.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Tourze\Symfony\Async\DependencyInjection\Compiler;
+
+use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Tourze\Symfony\Async\Message\AsyncMessageInterface;
+
+/**
+ * 减少一些不必要的服务注册
+ */
+class RemoveUnusedServicePass implements CompilerPassInterface
+{
+    public function process(ContainerBuilder $container): void
+    {
+        foreach ($container->getServiceIds() as $serviceId) {
+            $definition = $container->findDefinition($serviceId);
+            if (empty($definition->getClass())) {
+                continue;
+            }
+
+            try {
+                if (!class_exists($definition->getClass())) {
+                    continue;
+                }
+            } catch (\Throwable) {
+                continue;
+            }
+
+            // 请求不需要注册
+            if (is_subclass_of($definition->getClass(), AsyncMessageInterface::class)) {
+                $container->removeDefinition($serviceId);
+                continue;
+            }
+        }
+    }
+}
